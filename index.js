8822e442d5e303bb1b94a726cf73a98b7a7b0eec
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-02-02 21:18:44 +0700

    use getPendingCake inside index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const { reminderMessage } = require('./flexMessage/reminderMessage');
 
+const { getPendingCake } = require('./contractCall');
+
 const app = express();
 app.use(bodyParser.json());
 const fetch = require('node-fetch');
@@ -18,7 +20,22 @@ app.post('/', async (req, res) => {
 	const body = req.body;
 	switch (body.events[0].message.type) {
 		case 'text':
-			if (body.events[0].message.text === 'Sure, Did!') {
+			// getPendingCake
+			const msgText = body.events[0].message.text;
+			if (msgText === 'Sure, Did!') {
+				reply(body, [
+					{
+						type: 'sticker',
+						id: '13288487045964',
+						stickerId: '4',
+						packageId: '1',
+						stickerResourceType: 'STATIC',
+					},
+                ]);
+				break;
+			} else if (msgText === 'Cake') {
+				const pendingCake = await getPendingCake();
+				console.log(pendingCake);
 				reply(body, [
 					{
 						type: 'sticker',
